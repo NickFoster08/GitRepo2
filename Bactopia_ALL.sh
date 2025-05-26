8a22e7d3790161acce7013e1237a8460c41c4ad9
NickFoster08 <nf26742@uga.edu>
2025-05-26 16:20:16 -0400

    change pe pattern

--- Bactopia_ALL.sh
+++ Bactopia_ALL.sh
@@ -30,8 +30,8 @@ cd "$OUTDIR"
 bactopia prepare \
   --path /home/nf26742/All_Seqs/All_Renamed \
   --fastq-ext .fastq \
-  --pe1_pattern '_1.fastq' \
-  --pe2_pattern '_2.fastq' \
+  --pe1-pattern '_1.fastq' \
+  --pe2-pattern '_2.fastq' \
   --species "Mycobacterium bovis" \
   --genome-size 4400000 \
   > All.Country.Samples.txt
