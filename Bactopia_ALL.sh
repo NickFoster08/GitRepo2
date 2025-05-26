e540b6d1f8875b6bc491df1cc3cae10d921805fc
NickFoster08 <nf26742@uga.edu>
2025-05-26 16:54:45 -0400

    update smaple preparation

--- Bactopia_ALL.sh
+++ Bactopia_ALL.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#SBATCH --job-name=All_Country_Bactopia        # Job name
-#SBATCH --partition=batch                       # Partition (queue) name
-#SBATCH --ntasks=1                              # Run on a single CPU task
-#SBATCH --cpus-per-task=4                       # Number of cores per task
-#SBATCH --mem=40gb                              # Job memory request
-#SBATCH --time=02-00:00:00                      # Time limit hrs:min:sec
-#SBATCH --output=/scratch/nf26742/scratch/log.%j.out    # Standard output log
-#SBATCH --error=/scratch/nf26742/scratch/log.%j.err     # Standard error log
-#SBATCH --mail-type=END,FAIL                     # Mail events (NONE, BEGIN, END, FAIL, ALL)
-#SBATCH --mail-user=nf26742@uga.edu              # Where to send mail
+#SBATCH --job-name=All_Country_Bactopia
+#SBATCH --partition=batch
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=40gb
+#SBATCH --time=02-00:00:00
+#SBATCH --output=/scratch/nf26742/scratch/log.%j.out
+#SBATCH --error=/scratch/nf26742/scratch/log.%j.err
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=nf26742@uga.edu
 
 set -euo pipefail
 
@@ -16,9 +16,7 @@ set -euo pipefail
 OUTDIR="/scratch/nf26742/rerun_2025/job_${SLURM_JOB_ID}"
 
 # Make output directory if it doesn't exist
-if [ ! -d "$OUTDIR" ]; then
-    mkdir -p "$OUTDIR"
-fi
+mkdir -p "$OUTDIR"
 
 # Load modules
 module load Bactopia/3.1.0
@@ -34,11 +32,11 @@ bactopia prepare \
   --pe2-pattern '_2.fastq' \
   --species "Mycobacterium bovis" \
   --genome-size 4400000 \
-  > All.Country.Samples.txt
+  --output prepare_output
 
 # Run Bactopia
 bactopia \
-  --samples All.Country.Samples.txt \
+  --samples prepare_output/samples.csv \
   --coverage 100 \
   --max_cpus 4 \
   --outdir "$OUTDIR"
