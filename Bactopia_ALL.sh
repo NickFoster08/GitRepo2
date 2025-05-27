56d3519fffe0eadc1a6f1d5fcda1151c849d54bf
NickFoster08 <nf26742@uga.edu>
2025-05-27 12:56:53 -0400

    changed to smaples

--- Bactopia_ALL.sh
+++ Bactopia_ALL.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-#SBATCH --job-name=All_Country_Bactopia
-#SBATCH --partition=batch
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=4
-#SBATCH --mem=40gb
-#SBATCH --time=02-00:00:00
-#SBATCH --output=/scratch/nf26742/scratch/log.%j.out
-#SBATCH --error=/scratch/nf26742/scratch/log.%j.err
-#SBATCH --mail-type=END,FAIL
-#SBATCH --mail-user=nf26742@uga.edu
+#SBATCH --job-name= BactopiaAll    
+#SBATCH --partition=batch             # Partition (queue) name
+#SBATCH --ntasks=1                    # Run on a single CPU
+#SBATCH --cpus-per-task=4             #number of cores per task
+#SBATCH --mem=40gb                     # Job memory request
+#SBATCH --time=02-00:00:00               # Time limit hrs:min:sec
+#SBATCH --output=/scratch/nf26742/scratch/log.%j.out    # Standard output log
+#SBATCH --error=/scratch/nf26742/scratch/log.%j.err     # Standard error log
+
+#SBATCH --mail-type=END,FAIL          # Mail events (NONE, BEGIN, END, FAIL, ALL)
+#SBATCH --mail-user=nf26742@uga.edu  # Where to send mail (change username@uga.edu to your email address)
 
 set -euo pipefail
 
@@ -28,14 +29,16 @@ cd "$OUTDIR"
 bactopia prepare \
   --path /home/nf26742/All_Seqs/All_Renamed \
   --fastq-ext .fastq \
-  --pe1-pattern '_1.fastq' \
-  --pe2-pattern '_2.fastq' \
+ --pe1-pattern'_1.fastq' \
+ --pe2-pattern '_2.fastq' \
   --species "Mycobacterium bovis" \
-  --genome-size 4400000 
+  --genome-size 4400000 \
+  > $OUTDIR/samples.txt
   
 # Run Bactopia
 bactopia \
-  --path /home/nf26742/All_Seqs/All_Renamed \
-  --coverage 100 \
-  --max_cpus 4 \
-  --outdir "$OUTDIR"
+ --samples $OUTDIR/samples.txt \
+ --coverage 100 \
+ --outdir $OUTDIR/local-multiple-samples \
+ --max_cpus 4 \
+ --outdir "$OUTDIR"
