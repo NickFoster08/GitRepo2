bfd0ba709e6f24b6d6d54d6a4b4700453c617dde
NickFoster08 <nf26742@uga.edu>
2025-06-12 13:03:06 -0400

    initial

--- WF_Snippy.sh
+++ WF_Snippy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=Bactopia_Bioproject        # Job name
+#SBATCH --job-name=Bactopia_Step3_SNIPPY      # Job name
 #SBATCH --partition=batch             # Partition (queue) name
 #SBATCH --ntasks=1                    # Run on a single CPU
 #SBATCH --cpus-per-task=4             #number of cores per task
@@ -11,29 +11,25 @@
 #SBATCH --mail-type=END,FAIL          # Mail events (NONE, BEGIN, END, FAIL, ALL)
 #SBATCH --mail-user=nf26742@uga.edu  # Where to send mail (change username@uga.edu to your email address)
 
+REFERENCE=/home/nf26742/vsnp3_test_dataset/vsnp_dependencies/Mycobacterium_AF2122
 
-# Set output directory variable
-OUTDIR="/scratch/nf26742/rerun_2025/job_${SLURM_JOB_ID}"
+OUTDIR=/scratch/nf26742/rerun_2025/job_37864473
 
-# Make output directory if it doesn't exist
-mkdir -p "$OUTDIR"
+#Tells the program to make the ourdir folder if it cant find it
+if [ ! -d $OUTDIR ] 
+then
+    mkdir -p $OUTDIR
+fi
 
-# Load modules
+#Load modules
 module load Bactopia/3.1.0
 
-# Move to working directory
-cd "$OUTDIR"
+#move to workdir
+cd $OUTDIR
 
-# Prepare FOFN
-bactopia prepare \
-  --path /lustre2/scratch/nf26742/AllFastqs_UPDATED25 \
-  --species "Mycobacterium bovis" \
-  --genome-size 4400000 \
-  > $OUTDIR/samples.txt
-  
-# Run Bactopia
+#Bactopia tools
 bactopia \
- --samples $OUTDIR/samples.txt \
- --coverage 100 \
- --max_cpus 4 \
- --outdir "$OUTDIR"
+    --wf snippy \
+    --refrence $REFERENCE \
+    --exclude $OUTDIR/bactopia-exclude.tsv \
+    --bactopia $OUTDIR
