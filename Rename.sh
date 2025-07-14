6b502f336d55b3f78b1e7ec1094838c0c59c220c
NickFoster08 <nf26742@uga.edu>
2025-07-14 15:00:20 -0400

    added eheader

--- Rename.sh
+++ Rename.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+#SBATCH --job-name=Spain_Rename      # Job name
+#SBATCH --partition=batch                # Partition (queue) name
+#SBATCH --ntasks=1                       # Run on a single CPU
+#SBATCH --cpus-per-task=8                # Number of cores per task
+#SBATCH --mem=80gb                       # Job memory request
+#SBATCH --time=00-12:00:00               # Time limit hrs:min:sec
+#SBATCH --output=/scratch/nf26742/scratch/log.%j.out  # Standard output log
+#SBATCH --error=/scratch/nf26742/scratch/log.%j.err   # Standard error log
+
+#SBATCH --mail-type=END,FAIL             # Mail events (NONE, BEGIN, END, FAIL, ALL)
+#SBATCH --mail-user=nf26742@uga.edu      # Where to send mail
+
+# Specify output directory
+OUTDIR="/scratch/nf26742/Spain_WL"
 
 # Path to metadata file
 METADATA_FILE="/scratch/nf26742/Spain_WL/Full_Spain_WL_List.txt"
