b5c483535f17aab46912bc088f5da99cba48feaf
NickFoster08 <nf26742@uga.edu>
2025-12-01 16:27:20 -0500

    tb profiler is being moody

--- tbprofiler_force.sh
+++ tbprofiler_force.sh
@@ -1,30 +1,39 @@
 #!/bin/bash
-#SBATCH --job-name=TB_Profiler_USA_BOVIS     # Job name
-#SBATCH --partition=batch             # Partition (queue) name
-#SBATCH --ntasks=1                    # Run on a single CPU
-#SBATCH --cpus-per-task=4             #number of cores per task
-#SBATCH --mem=40GB                     # Job memory request
-#SBATCH --time=07-00:00:00               # Time limit hrs:min:sec
-#SBATCH --output=/scratch/nf26742/scratch/log.%j.out    # Standard output log
-#SBATCH --error=/scratch/nf26742/scratch/log.%j.err     # Standard error log
+#SBATCH --job-name=TB_Profiler_USA_BOVIS
+#SBATCH --partition=batch
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=40GB
+#SBATCH --time=48:00:00
+#SBATCH --output=/scratch/nf26742/scratch/log.%j.out
+#SBATCH --error=/scratch/nf26742/scratch/log.%j.err
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=nf26742@uga.edu
 
-#SBATCH --mail-type=END,FAIL          # Mail events (NONE, BEGIN, END, FAIL, ALL)
-#SBATCH --mail-user=nf26742@uga.edu  # Where to send mail (change username@uga.edu to your email address)
-
-#Specify outdir
-OUTDIR=/scratch/nf26742/USA_Bovis_Human/results_3
-
-#Tells the program to make the ourdir folder if it cant find it
-if [ ! -d $OUTDIR ] 
-then
-    mkdir -p $OUTDIR
-fi
-
-#Navigate to fastqdir
-cd /scratch/nf26742/USA_Bovis_Human
-
-#load TBprofiler
+# Load module
 module load TB-Profiler/6.6.5
 
-#invoke tbprofiler
-tb-profiler profile -1 *_1.fastq.gz -2 *_2.fastq.gz -o $OUTDIR -t 4
+# Directory containing FASTQs
+FASTQDIR=/scratch/nf26742/USA_Bovis_Human
+OUTDIR=/scratch/nf26742/USA_Bovis_Human/results_3
+
+mkdir -p $OUTDIR
+
+cd $FASTQDIR
+
+# Loop over all _1.fastq.gz files
+for fq1 in *_1.fastq.gz; do
+    # Infer _2 file
+    fq2="${fq1/_1.fastq.gz/_2.fastq.gz}"
+    
+    # Generate a prefix based on the sample name
+    prefix="${fq1%%_1.fastq.gz}"
+    
+    echo "Processing $prefix ..."
+    
+    tb-profiler profile -1 "$fq1" -2 "$fq2" -o "$OUTDIR" -t 4 -p "$prefix"
+done
+
+# Collate results at the end
+cd $OUTDIR
+tb-profiler collate
