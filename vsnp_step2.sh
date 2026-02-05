87c2a46971e0115ff79af7627ead6d77ceecd933
NickFoster08 <nf26742@uga.edu>
2026-02-05 13:36:13 -0500

    initial

--- vsnp_step2.sh
+++ vsnp_step2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=VSNP_STEP2_USA        # Job name
+#SBATCH --job-name=vSNP_STEP2_USA        # Job name
 #SBATCH --partition=batch             # Partition (queue) name
 #SBATCH --ntasks=1                    # Run on a single CPU
 #SBATCH --cpus-per-task=4             #number of cores per task
