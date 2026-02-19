c9ef7347607e12753161a25f54eaa7aa30b72570
NickFoster08 <nf26742@uga.edu>
2026-02-19 14:28:27 -0500

    updated header names

--- Metadata_Rename_TSV.sh
+++ Metadata_Rename_TSV.sh
@@ -50,10 +50,10 @@ echo "Columns found:"
 echo "$header" | tr '\t' '\n' | nl
 
 #Define all columns BEFORE the check
-country_col=$(echo "$header" | tr '\t' '\n' | grep -n -i '^geo_loc_name_country$' | cut -d: -f1)
-date_col=$(echo "$header" | tr '\t' '\n' | grep -n -i '^Collection_Date$' | cut -d: -f1)
-host_col=$(echo "$header" | tr '\t' '\n' | grep -n -i '^HOST$' | cut -d: -f1)
-run_col=$(echo "$header" | tr '\t' '\n' | grep -n -i '^Run$' | cut -d: -f1)
+country_col=$(echo "$header" | tr '\t' '\n' | grep -n -i '^country$' | cut -d: -f1)
+date_col=$(echo "$header" | tr '\t' '\n' | grep -n -i '^collection_Date$' | cut -d: -f1)
+host_col=$(echo "$header" | tr '\t' '\n' | grep -n -i '^host$' | cut -d: -f1)
+run_col=$(echo "$header" | tr '\t' '\n' | grep -n -i '^accession_id$' | cut -d: -f1)
 
 if [[ -z $geo_col || -z $host_col || -z $date_col || -z $run_col ]]; then
     echo "❌ Error: Could not find one or more required columns in the metadata header."
