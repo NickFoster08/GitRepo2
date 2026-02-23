ff79e6035a788a95c57ca3d208a156dc8311ed44
NickFoster08 <nf26742@uga.edu>
2026-02-23 13:58:44 -0500

    update tsv

--- Metadata_Rename_TSV.sh
+++ Metadata_Rename_TSV.sh
@@ -13,7 +13,7 @@
 set -euo pipefail
 
 OUTDIR="/lustre2/scratch/nf26742/Mex_USA_Animal_Bovis"
-METADATA="/lustre2/scratch/nf26742/Mex_USA_Animal_Bovis/Mexico_USA_Metadata.tsv"
+METADATA="/lustre2/scratch/nf26742/Mex_USA_Animal_Bovis/Mexico_USA_Metadata_clean.tsv"
 
 echo "Running script: $0"
 echo "Working directory: $(pwd)"
@@ -29,9 +29,9 @@ header=$(head -n1 "$METADATA")
 echo "Header: $header"
 
 # Detect column positions
-run_col=$(echo "$header" | awk -F',' '{for(i=1;i<=NF;i++) if($i=="Run_") print i}')
-date_col=$(echo "$header" | awk -F',' '{for(i=1;i<=NF;i++) if($i=="Collection_Date_") print i}')
-country_col=$(echo "$header" | awk -F',' '{for(i=1;i<=NF;i++) if($i=="geo_loc_name_") print i}')
+run_col=$(echo "$header" | awk -F',' '{for(i=1;i<=NF;i++) if($i=="Run") print i}')
+date_col=$(echo "$header" | awk -F',' '{for(i=1;i<=NF;i++) if($i=="Collection_Date") print i}')
+country_col=$(echo "$header" | awk -F',' '{for(i=1;i<=NF;i++) if($i=="geo_loc_name") print i}')
 host_col=$(echo "$header" | awk -F',' '{for(i=1;i<=NF;i++) if($i=="HOST") print i}')
 
 if [[ -z "$run_col" || -z "$date_col" || -z "$country_col" || -z "$host_col" ]]; then
