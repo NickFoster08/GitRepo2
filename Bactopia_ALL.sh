fa8fa2a8dc72e4a94df188ba7cf3dcec45d7faec
NickFoster08 <nf26742@uga.edu>
2026-03-26 16:13:57 -0400

    parallel

--- Bactopia_ALL.sh
+++ Bactopia_ALL.sh
@@ -1,39 +1,32 @@
 #!/bin/bash
-#SBATCH --job-name=Bactopia_USA_Human_Bovis        # Job name
-#SBATCH --partition=batch             # Partition (queue) name
-#SBATCH --ntasks=1                    # Run on a single CPU
-#SBATCH --cpus-per-task=4             #number of cores per task
-#SBATCH --mem=40GB                     # Job memory request
-#SBATCH --time=07-00:00:00               # Time limit hrs:min:sec
-#SBATCH --output=/scratch/nf26742/scratch/log.%j.out    # Standard output log
-#SBATCH --error=/scratch/nf26742/scratch/log.%j.err     # Standard error log
-
-#SBATCH --mail-type=END,FAIL          # Mail events (NONE, BEGIN, END, FAIL, ALL)
-#SBATCH --mail-user=nf26742@uga.edu  # Where to send mail (change username@uga.edu to your email address)
-
-
-# Set output directory variable
-OUTDIR="/scratch/nf26742/rerun_2025/job_${SLURM_JOB_ID}"
-
-# Make output directory if it doesn't exist
-mkdir -p "$OUTDIR"
+#SBATCH --job-name=Bactopia_USA_Human_Bovis
+#SBATCH --partition=batch
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=40GB
+#SBATCH --time=07-00:00:00
+#SBATCH --output=/scratch/nf26742/scratch/log.%j.out
+#SBATCH --error=/scratch/nf26742/scratch/log.%j.err
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=nf26742@uga.edu
 
 # Load modules
 module load Bactopia/3.2.0
 
 # Move to working directory
-cd "$OUTDIR"
+cd /scratch/nf26742/rerun_2025
 
-# Prepare FOFN
+# Prepare FOFN (only needed once; can reuse samples.txt)
 bactopia prepare \
-  --path /lustre2/scratch/nf26742/Mex_USA_Animal_Bovis\
+  --path /lustre2/scratch/nf26742/Mex_USA_Animal_Bovis \
   --species "Mycobacterium bovis" \
   --genome-size 4400000 \
-  > $OUTDIR/samples.txt
-  
-# Run Bactopia
+  > samples.txt
+
+# Run Bactopia with SLURM profile and resume previous run
 bactopia \
- --samples $OUTDIR/samples.txt \
- --coverage 100 \
- --max_cpus 4 \
- --outdir "$OUTDIR"
+  --samples samples.txt \
+  --coverage 100 \
+  --outdir ./bactopia_output \
+  -profile slurm \
+  -resume
