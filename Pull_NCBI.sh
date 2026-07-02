bea3e96299ab724bf0b5a2631cd16e4127f1fe0e
NickFoster08 <nf26742@uga.edu>
2026-07-02 16:20:47 -0400

    morocco

--- Pull_NCBI.sh
+++ Pull_NCBI.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=USA_CDC_Bov      # Job name
+#SBATCH --job-name=NCBI_Download_SRR_Accession      # Job name
 #SBATCH --partition=batch                # Partition (queue) name
 #SBATCH --ntasks=1                       # Run on a single CPU
 #SBATCH --cpus-per-task=8                # Number of cores per task
