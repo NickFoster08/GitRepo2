2e4b001c42da002a20abe4cfe8980b00e7b4e92b
NickFoster08 <nf26742@uga.edu>
2026-07-10 16:12:37 -0400

    portugla

--- Metadata_Rename_TSV.sh
+++ Metadata_Rename_TSV.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=TSV_Rename        # Job name
+#SBATCH --job-name=TSV_rename        # Job name
 #SBATCH --partition=batch                 # Partition (queue) name
 #SBATCH --ntasks=1                        # Run on a single CPU
 #SBATCH --cpus-per-task=8                 # Number of cores per task
