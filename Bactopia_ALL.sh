02813519dc6df01d47eea0cbed7c890df5253631
NickFoster08 <nf26742@uga.edu>
2026-08-26 14:31:44 -0400

    wall clock limit

--- Bactopia_ALL.sh
+++ Bactopia_ALL.sh
@@ -4,7 +4,7 @@
 #SBATCH --ntasks=1                  # Run on a single CPU
 #SBATCH --cpus-per-task=12       #number of cores per task
 #SBATCH --mem=120GB                     # Job memory request
-#SBATCH --time=12-00:00:00               # Time limit hrs:min:sec
+#SBATCH --time=7-00:00:00               # Time limit hrs:min:sec
 #SBATCH --output=/scratch/nf26742/scratch/log.%j.out    # Standard output log
 #SBATCH --error=/scratch/nf26742/scratch/log.%j.err     # Standard error log
 
