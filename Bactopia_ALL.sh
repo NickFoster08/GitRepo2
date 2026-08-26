eadc93d2b83445128c83c59c9551fc6c143d6553
NickFoster08 <nf26742@uga.edu>
2026-08-26 14:39:29 -0400

    typo fixes

--- Bactopia_ALL.sh
+++ Bactopia_ALL.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=Bactopia_Euro_Afri_Bovis        # Job name
+#SBATCH --job-name=Bactopia_Euro_Afr_Bovis        # Job name
 #SBATCH --partition=batch             # Partition (queue) name
 #SBATCH --ntasks=1                  # Run on a single CPU
 #SBATCH --cpus-per-task=12       #number of cores per task
