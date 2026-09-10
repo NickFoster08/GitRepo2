5e792d79bb3d9d57f6f69698935101754d6b8fab
NickFoster08 <nf26742@uga.edu>
2026-09-10 14:34:17 -0400

    tb

--- Bactopia_ALL.sh
+++ Bactopia_ALL.sh
@@ -3,7 +3,7 @@
 #SBATCH --partition=hugemem_p             # Partition (queue) name
 #SBATCH --ntasks=1                  # Run on a single CPU
 #SBATCH --cpus-per-task=32       #number of cores per task
-#SBATCH --mem=1 T                    # Job memory request
+#SBATCH --mem=1tb                   # Job memory request
 #SBATCH --time=07-0:00:00               # Time limit hrs:min:sec
 #SBATCH --output=/scratch/nf26742/scratch/log.%j.out    # Standard output log
 #SBATCH --error=/scratch/nf26742/scratch/log.%j.err     # Standard error log
