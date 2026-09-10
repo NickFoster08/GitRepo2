492fd755f1a0b114a6762a16087cf3a92993ab73
NickFoster08 <nf26742@uga.edu>
2026-09-10 16:15:53 -0400

    resource allocation

--- Bactopia_ALL.sh
+++ Bactopia_ALL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --job-name=Bactopia_Euro_Afr_Bovis        # Job name
-#SBATCH --partition=hugemem_p             # Partition (queue) name
+#SBATCH --partition=batch_30d             # Partition (queue) name
 #SBATCH --ntasks=1                  # Run on a single CPU
-#SBATCH --cpus-per-task=32       #number of cores per task
-#SBATCH --mem=1tb                   # Job memory request
-#SBATCH --time=07-0:00:00               # Time limit hrs:min:sec
+#SBATCH --cpus-per-task=16       #number of cores per task
+#SBATCH --mem=300gb                   # Job memory request
+#SBATCH --time=14-0:00:00               # Time limit hrs:min:sec
 #SBATCH --output=/scratch/nf26742/scratch/log.%j.out    # Standard output log
 #SBATCH --error=/scratch/nf26742/scratch/log.%j.err     # Standard error log
 
