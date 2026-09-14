768dc5ff7f22281875ca1a4a59df0ae3e0b12814
NickFoster08 <nf26742@uga.edu>
2026-09-14 16:36:46 -0400

    decrease resource

--- WF_TBprofiler.sh
+++ WF_TBprofiler.sh
@@ -3,8 +3,8 @@
 #SBATCH --partition=batch_30d             # Partition (queue) name
 #SBATCH --ntasks=1                  # Run on a single CPU
 #SBATCH --cpus-per-task=16       #number of cores per task
-#SBATCH --mem=300gb                   # Job memory request
-#SBATCH --time=14-0:00:00               # Time limit hrs:min:sec
+#SBATCH --mem=200gb                   # Job memory request
+#SBATCH --time=7-0:00:00               # Time limit hrs:min:sec
 #SBATCH --output=/scratch/nf26742/scratch/log.%j.out    # Standard output log
 #SBATCH --error=/scratch/nf26742/scratch/log.%j.err     # Standard error log
 
