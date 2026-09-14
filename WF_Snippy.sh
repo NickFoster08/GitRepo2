71e11a2595f960bc8237e9d01c8376c4baa56eea
NickFoster08 <nf26742@uga.edu>
2026-09-14 16:34:57 -0400

    path update

--- WF_Snippy.sh
+++ WF_Snippy.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
-#SBATCH --job-name=SNIPPY_Americas_BOVIS     # Job name
-#SBATCH --partition=batch             # Partition (queue) name
-#SBATCH --ntasks=1                    # Run on a single CPU
-#SBATCH --cpus-per-task=12            #number of cores per task
-#SBATCH --mem=120GB                     # Job memory request
-#SBATCH --time=07-00:00:00               # Time limit hrs:min:sec
+#SBATCH --job-name=WF_Snippy_Euro_Afr_Bovis        # Job name
+#SBATCH --partition=batch_30d             # Partition (queue) name
+#SBATCH --ntasks=1                  # Run on a single CPU
+#SBATCH --cpus-per-task=16       #number of cores per task
+#SBATCH --mem=300gb                   # Job memory request
+#SBATCH --time=14-0:00:00               # Time limit hrs:min:sec
 #SBATCH --output=/scratch/nf26742/scratch/log.%j.out    # Standard output log
 #SBATCH --error=/scratch/nf26742/scratch/log.%j.err     # Standard error log
 
 #SBATCH --mail-type=END,FAIL          # Mail events (NONE, BEGIN, END, FAIL, ALL)
 #SBATCH --mail-user=nf26742@uga.edu  # Where to send mail (change username@uga.edu to your email address)
+#Specify outdir
 
 #Exit on error immediatley 
 set -e
 
 REFERENCE=/home/nf26742/vsnp3_test_dataset/vsnp_dependencies/Mycobacterium_AF2122
 
-OUTDIR=/scratch/nf26742/rerun_2025/job_43913418
+OUTDIR=/scratch/nf26742/rerun_2025/job_47709570
 
 #Tells the program to make the ourdir folder if it cant find it
 if [ ! -d $OUTDIR ] 
