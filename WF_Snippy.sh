ea25deae7a000239fd4ad4319366ed652737b599
NickFoster08 <nf26742@uga.edu>
2026-09-22 17:00:32 -0400

    update path

--- WF_Snippy.sh
+++ WF_Snippy.sh
@@ -17,7 +17,7 @@ set -e
 
 REFERENCE=/home/nf26742/vsnp3_test_dataset/vsnp_dependencies/Mycobacterium_AF2122
 
-OUTDIR=/scratch/nf26742/rerun_2025/job_47709570
+OUTDIR=/scratch/nf26742/rerun_2025/job_47709570/bactopia-runs/snippy-20260914-163539
 
 #Tells the program to make the ourdir folder if it cant find it
 if [ ! -d $OUTDIR ] 
