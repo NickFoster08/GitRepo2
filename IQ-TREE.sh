1c5625ffe6f3336678e264b12aa63d6c29082215
NickFoster08 <nf26742@uga.edu>
2026-09-23 14:45:34 -0400

    request icnrease

--- IQ-TREE.sh
+++ IQ-TREE.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --job-name=IQTREE_EUR_AFR_BOVIS     # Job name
-#SBATCH --partition=batch             # Partition (queue) name
-#SBATCH --ntasks=1                    # Run on a single CPU
-#SBATCH --cpus-per-task=4             #number of cores per task
-#SBATCH --mem=700GB                     # Job memory request
+#SBATCH --partition=highmem_30d_p           # Partition (queue) name
+#SBATCH --ntasks=1                  # Run on a single CPU
+#SBATCH --cpus-per-task=16       #number of cores per task
+#SBATCH --mem=600gb                   # Job memory request
 #SBATCH --time=07-00:00:00               # Time limit hrs:min:sec
 #SBATCH --output=/scratch/nf26742/scratch/log.%j.out    # Standard output log
 #SBATCH --error=/scratch/nf26742/scratch/log.%j.err     # Standard error log
