149a0e98966841a90a88d7cddc90da67e48e8135
NickFoster08 <nf26742@uga.edu>
2026-09-23 14:55:28 -0400

    resoruce estimation again

--- IQ-TREE.sh
+++ IQ-TREE.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --job-name=IQTREE_EUR_AFR_BOVIS     # Job name
-#SBATCH --partition=highmem_p          # Partition (queue) name
+#SBATCH --partition=batch         # Partition (queue) name
 #SBATCH --ntasks=1                  # Run on a single CPU
-#SBATCH --cpus-per-task=16       #number of cores per task
-#SBATCH --mem=600gb                   # Job memory request
+#SBATCH --cpus-per-task=4     #number of cores per task
+#SBATCH --mem=180gb                   # Job memory request
 #SBATCH --time=07-00:00:00               # Time limit hrs:min:sec
 #SBATCH --output=/scratch/nf26742/scratch/log.%j.out    # Standard output log
 #SBATCH --error=/scratch/nf26742/scratch/log.%j.err     # Standard error log
@@ -31,6 +31,11 @@ fi
 module load IQ-TREE/3.0.1-gompi-2024a
 
 # Run IQ-TREE with outputs going into the clean folder
-iqtree3 -s core-snp-clean.full.aln -m GTR -bb 1000 -nt 4 -pre $OUTDIR/iqtree_full
+iqtree3 \
+ -s core-snp-clean.full.aln \
+ -m GTR \
+ -bb 1000 \ 
+ -nt 4 \
+ -pre $OUTDIR/iqtree_full
 
 
