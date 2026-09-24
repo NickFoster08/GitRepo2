5ba276b6a4fab78faf1683bb67b57c44259dc4f2
NickFoster08 <nf26742@uga.edu>
2026-09-24 15:06:16 -0400

    change file type

--- IQ-TREE.sh
+++ IQ-TREE.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --job-name=HM_IQTREE_EUR_AFR_BOVIS     # Job name
-#SBATCH --partition=hugemem_p     # Partition (queue) name
+#SBATCH --partition=highmem_p     # Partition (queue) name
 #SBATCH --ntasks=1                  # Run on a single CPU
-#SBATCH --cpus-per-task=4     #number of cores per task
-#SBATCH --mem=1tb                  # Job memory request
+#SBATCH --cpus-per-task=16     #number of cores per task
+#SBATCH --mem=800gb                  # Job memory request
 #SBATCH --time=07-00:00:00               # Time limit hrs:min:sec
 #SBATCH --output=/scratch/nf26742/scratch/log.%j.out    # Standard output log
 #SBATCH --error=/scratch/nf26742/scratch/log.%j.err     # Standard error log
@@ -22,8 +22,8 @@ mkdir -p $OUTDIR
 cd /scratch/nf26742/rerun_2025/job_47709570/bactopia-runs/snippy-20260914-163539/
 
 # Check that the cleaned alignment exists
-if [ ! -f core-snp-clean.full.aln ]; then
-    echo "Error: core-snp-clean.full.aln not found in this folder"
+if [ ! -f core-snp-variable-raw.aln ]; then
+    echo "Error: core-snp-variable-raw.aln not found in this folder"
     exit 1
 fi
 
@@ -32,10 +32,9 @@ module load IQ-TREE/3.0.1-gompi-2024a
 
 # Run IQ-TREE with outputs going into the clean folder
 iqtree3 \
- -s core-snp-clean.full.aln \
+ -s core-snp-variable-raw.aln \
  -m HKY+G4 \
- -bb 1000 \
- -nt 4 \
+ -nt 16 \
  -pre "$OUTDIR/iqtree_full"
 
 
